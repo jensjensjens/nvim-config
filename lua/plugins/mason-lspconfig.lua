a087d8eb4af91edb0412c3e2528d0cb63218b7b1
Jens Sigvardsson <19902144+jensjensjens@users.noreply.github.com>
2024-02-24 08:12:50 +0100

    Added base settings to powershell es

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -94,6 +94,17 @@ local handlers = {
       },
     })
   end,
+  ["powershell_es"] = function()
+    require("lspconfig").powershell_es.setup({
+      settings = {
+        powershell = {
+          codeFormatting = {
+            Preset = "OTBS",
+          },
+        },
+      },
+    })
+  end,
   ["lua_ls"] = function()
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
     require("lspconfig").lua_ls.setup({
